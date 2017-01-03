40af813304d4da074504f70435e32b950dc09736
Ivan Salosin <salonkasoli@gmail.com>
2017-01-03 23:08:37 +0700

    Convert server.js to module

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var http = require("http");
 
-function onRequest(request, response) {
-    console.log("Request accepted");
+
+function start(){
+
+    function onRequest(request, response) {
+       console.log("Request accepted");
+    }
+
+    http.createServer(onRequest).listen(1337);
+
+    console.log("Server starts");
 }
 
-http.createServer(onRequest).listen(1337);
-
-console.log("Server starts");
+exports.start = start;
