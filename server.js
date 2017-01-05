1a33957f9bad1074f56a69b3a9c34630bf850e52
Eva <d135227@gmail.com>
2017-01-06 01:20:38 +0700

    Add requestHandler

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require("http");
-
+var requestHandler = require("./requestHandler");
 
 function start(){
-
-    function onRequest(request, response) {
-       console.log("Request accepted");
-    }
+	function onRequest(request, response){
+		console.log("Request accepted");
+    		requestHandler.toRequest(request, response);
+	}
 
     http.createServer(onRequest).listen(1337);
 
